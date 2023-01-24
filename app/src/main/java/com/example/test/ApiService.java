e9b0e433ea7289b0718015ba7c95ce3d433a031e
SarkisovDavid <kingdavid23@mail.ru>
2023-01-24 15:04:15 +0300

    Add MainViewModel; add LiveData

--- app/src/main/java/com/example/test/ApiService.java
+++ app/src/main/java/com/example/test/ApiService.java
@@ -2,9 +2,12 @@ package com.example.test;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
     @GET("45717360")
     Single<Card> cardInfo();
+
+//    @Query("") String cardNumber
 }
