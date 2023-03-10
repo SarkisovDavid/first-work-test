30f033e212a1cdd8635ce9f7898813ddc99c8117
SarkisovDavid <kingdavid23@mail.ru>
2023-03-10 08:58:11 +0300

    Revert "Исправление замечаний"
    This reverts commit 12039e61c2509632cf261b765fd8bef23bd0d9fb.

--- app/src/main/java/com/example/test/ConverterMAP.java
+++ app/src/main/java/com/example/test/ConverterMAP.java
@@ -21,16 +21,14 @@ public class ConverterMAP {
     }
 
     private String countryInfo(Country country) {
-        String cardCountryInfo = "";
-        if (country != null) {
-            if (country.getAlpha2() != null) {
-                cardCountryInfo = cardCountryInfo + country.getAlpha2();
-            }
-            if (!cardCountryInfo.isEmpty()) {
-                cardCountryInfo = cardCountryInfo + " ";
-            }
-            if (country.getName() != null) {
-                cardCountryInfo = cardCountryInfo + country.getName();
+        String cardCountryInfo = null;
+        if(country != null) {
+            if (country.getAlpha2() != null && country.getName() != null) {
+                cardCountryInfo = country.getAlpha2() + " " + country.getName();
+            } else if (country.getName() != null) {
+                cardCountryInfo = country.getName();
+            } else if (country.getAlpha2() != null) {
+                cardCountryInfo = country.getAlpha2();
             }
         }
         return cardCountryInfo;
@@ -38,14 +36,12 @@ public class ConverterMAP {
 
     private String bankInfo(Card card) {
         String cardBankNameCity = "";
-        if (card.getBank().getName() != null) {
-            cardBankNameCity = cardBankNameCity + card.getBank().getName();
-        }
-        if (!cardBankNameCity.isEmpty()) {
-            cardBankNameCity = cardBankNameCity + " ";
-        }
-        if (card.getBank().getCity() != null) {
-            cardBankNameCity = cardBankNameCity + card.getBank().getCity();
+        if (card.getBank().getName() != null && card.getBank().getCity() != null) {
+            cardBankNameCity = card.getBank().getName() + ", " + card.getBank().getCity();
+        } else if (card.getBank().getCity() != null) {
+            cardBankNameCity = card.getBank().getCity();
+        } else if (card.getBank().getName() != null) {
+            cardBankNameCity = card.getBank().getName();
         }
         return cardBankNameCity;
     }
@@ -67,7 +63,8 @@ public class ConverterMAP {
         if (predicate != null) {
             if (predicate) {
                 result = trueString;
-            } else {
+            }
+            else {
                 result = falseString;
             }
         }
