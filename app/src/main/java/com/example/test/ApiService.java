cd29bc940633876ef0d3517ac5e63ddfc313580c
SarkisovDavid <kingdavid23@mail.ru>
2023-03-10 09:16:12 +0300

    Revert "Добавлен map() и flatmap() #3"
    This reverts commit 70c9465a62cab9d8044fd9ebc125704d84c88478.

--- app/src/main/java/com/example/test/ApiService.java
+++ app/src/main/java/com/example/test/ApiService.java
@@ -1,7 +1,9 @@
 package com.example.test;
 
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface ApiService {
 
--- app/src/main/java/com/example/test/MainViewModel.java
+++ app/src/main/java/com/example/test/MainViewModel.java
@@ -1,6 +1,7 @@
 package com.example.test;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
@@ -10,17 +11,13 @@ import androidx.lifecycle.MutableLiveData;
 import java.util.Map;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Function;
-import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainViewModel extends AndroidViewModel {
 
-    private final ConverterMAP converterMAP = new ConverterMAP();
+    private final ConverterMAP mConverterMAP = new ConverterMAP();
     private static final String TAG = "MainViewModel";
     private final MutableLiveData<Map<String, String>> cardMutableLiveData = new MutableLiveData<>();
     private final CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -37,29 +34,11 @@ public class MainViewModel extends AndroidViewModel {
     public void loadCardInfo() {
         Disposable disposable = ApiFactory.apiService.cardInfo()
                 .subscribeOn(Schedulers.io())
-                .map(new Function<Card, Map<String, String>>() {
-                    @Override
-                    public Map<String, String> apply(Card card){
-                        return converterMAP.convert(card);
-                    }
-                })
-                .flatMapObservable(new Function<Map<String, String>, ObservableSource<Map.Entry<String,String>>>() {
-                                       @Override
-                                       public ObservableSource<Map.Entry<String, String>> apply(Map<String, String> stringStringMap) throws Throwable {
-                                           return Observable.fromIterable(stringStringMap.entrySet());
-                                       }
-                                   })
-                .filter(new Predicate<Map.Entry<String, String>>() {
-                    @Override
-                    public boolean test(Map.Entry<String, String> stringStringEntry) {
-                        return false;
-                    }
-                })
                 .observeOn(AndroidSchedulers.mainThread())
-                // .subscribe(
-                //         cardInfoItems -> Log.d("MainViewModel", cardInfoItems.getValue()),
-                //         throwable -> Log.d("MainActivity", throwable.toString())
-                // );
+                .subscribe(
+                        card -> cardMutableLiveData.setValue(mConverterMAP.convert(card)),
+                        throwable -> Log.d("MainActivity", throwable.toString())
+                );
         compositeDisposable.add(disposable);
     }
 
